95330a47c1619892814598cedc134a046de08f7d
Nikola Dichev <nikola.dichev@gmail.com>
2023-04-14 13:20:31 +0300

    Set up Spring stuff.

---
+++ src/main/java/com/nikoladichev/financialreportanalyzer/FinancialReportAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.nikoladichev.financialreportanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialReportAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialReportAnalyzerApplication.class, args);
+	}
+
+}
